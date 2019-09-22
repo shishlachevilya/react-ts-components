05c22bf51a05f952a6e0ba5d8b83da13ff81740a
Shishlachev-ilya <mailforwot43@mail.ru>
2019-09-22 19:36:43 +0300

    added button component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,48 @@
 import React from 'react';
+import Button from '../Button';
 
 import './App.css';
 
 const App: React.FC = () => {
   return (
-    <h1>Hello world!</h1>
+    <React.Fragment>
+      <Button
+        onClick={() => console.log('click')}
+        label="default"
+        className="custom-class"
+      />
+
+      <Button
+        onClick={() => console.log('click')}
+        label="active"
+        active={ true }
+      />
+
+      <Button
+        onClick={() => console.log('click')}
+        label="href"
+        href="https://www.google.com"
+        target="_blank"
+      />
+
+      <Button
+        onClick={() => console.log('click')}
+        label="href"
+        disabled
+      />
+
+      <Button
+        onClick={() => console.log('click')}
+        label="disabled"
+        disabled
+      />
+
+      <Button
+        onClick={() => console.log('click')}
+        label="data"
+        data-action="add"
+      />
+    </React.Fragment>
   );
 };
 
